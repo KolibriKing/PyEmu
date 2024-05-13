e053893a3dff2a1916cfbf768b3212233f34096b
KolibriKing12 <maksimevstratiev12@outlook.com>
2024-05-13 15:25:50 +0300

    feature: ask os index

--- boot/bm.py
+++ boot/bm.py
@@ -9,10 +9,15 @@ if not spaces > 0 and (spaces & (spaces - 1)) == 0:
   spaces = spaces - 1
 print('\033[7m' + ' ' * int(spaces) + 'Boot menu' + ' ' * int(spaces) + '\033[0m')
 
+i = 0
+ilist = []
 for fname in listdir('boot/os'):
   f = path.join('boot/os', fname)
   if path.isfile(f):
     with open(f, 'r') as file:
       file_content = file.readlines()
       if file_content[0][:16] == '#emu_boot def os':
-        print(file_content[0][17:])
+        print(f'[{i}] ' + file_content[0][17:])
+        i = i + 1
+        ilist.append(f)
+osindex = input('> ')
